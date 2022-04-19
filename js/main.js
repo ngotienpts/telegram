53f2403935a7d6cd05cffa438c08af5d5310e22b
unknown <ngotienpts@gmail.com>
2022-04-19 16:55:40 +0700

    19/4

--- js/main.js
+++ js/main.js
@@ -14,6 +14,10 @@ document.addEventListener("DOMContentLoaded", function () {
 
   var rightColumn = document.querySelector("#RightColumn-wrapper");
 
+  var popupAddContact = document.querySelectorAll("#popup-add-contact");
+
+  var popupNewContact = document.querySelectorAll("#popup-new-contact");
+
   const app = {
     // su ly cac su kien
     handleEvent: function () {
@@ -52,14 +56,44 @@ document.addEventListener("DOMContentLoaded", function () {
       // LeftColumn
       if (leftColumn) {
         var searchInput = leftColumn.querySelector(".SearchInput");
+
         var stateBack = leftColumn.querySelector(".animated-menu-icon");
+
         var dropdownMenu = leftColumn.querySelector(".DropdownMenu");
+
         var newChat = leftColumn.querySelector(".NewChatButton");
+
         var transitionSlide1 = leftColumn.querySelector("#slide-1");
+
         var transitionSlide2 = leftColumn.querySelector("#slide-2");
+
         var transitionSlide3 = leftColumn.querySelector("#slide-3");
+
         var createChat = leftColumn.querySelector("#create-chat");
+
         var chatItems = leftColumn.querySelectorAll(".chat-item");
+
+        var settingBlock = leftColumn.querySelector(".setting-wrapper");
+
+        var editSettingBlock = leftColumn.querySelector(".edit-setting-wrapper");
+
+        var showGeneral = leftColumn.querySelector(".GeneralSetting");
+
+        var generalSettingBlock = leftColumn.querySelector(".general-setting-wrapper");
+
+        var showSetting = leftColumn.querySelector(".submenu-setting");
+
+        var pushSlide = leftColumn.querySelector(".push-slide");
+
+        var closeLeftColumn = leftColumn.querySelectorAll(".close-left-column");
+
+        var showEditSetting = leftColumn.querySelector(".open-edit-setting");
+        
+        var showExtendSetting = leftColumn.querySelector(".LeftHeader__open-extend");
+
+        var extendSettingBlock = leftColumn.querySelector('.settings-more-menu');
+
+
 
         searchInput.querySelector("#telegram-search-input").onfocus =
           function () {
@@ -83,6 +117,10 @@ document.addEventListener("DOMContentLoaded", function () {
             }
           };
 
+
+
+
+          // 
         if (dropdownMenu) {
           var submenuContact = document.querySelector(".submenu-contact");
           var dropdownMenuContainer = document.querySelector(".sub-menu");
@@ -124,6 +162,11 @@ document.addEventListener("DOMContentLoaded", function () {
           }
         }
 
+
+
+
+
+
         //
         if (createChat) {
           var btnCreate = createChat.firstElementChild;
@@ -160,6 +203,9 @@ document.addEventListener("DOMContentLoaded", function () {
           }
         };
 
+
+
+        // 
         if (middleColumn) {
           var conversation = middleColumn.querySelector("#messages-layout");
           var backBtn = middleColumn.querySelectorAll(".back-btn");
@@ -181,6 +227,56 @@ document.addEventListener("DOMContentLoaded", function () {
             main.classList.remove("middle-column-open");
           };
         });
+         
+        showSetting.onclick = function(){
+          if(pushSlide.matches('.active')){
+            pushSlide.classList.remove('active');
+            main.classList.add("left-column-open");
+            settingBlock.classList.add("shown");
+          }
+        }
+
+
+        // general setting
+        showGeneral.onclick = function(){
+          if(settingBlock.matches('.shown')){
+            settingBlock.classList.remove('shown');
+            generalSettingBlock.classList.add("shown");
+          }
+        }
+
+        // 
+        if(closeLeftColumn){
+          closeLeftColumn.forEach(function(a){
+            a.onclick = function(){
+              if(main.matches('.left-column-open')){
+                main.classList.remove('left-column-open');
+                pushSlide.classList.add('active');
+              }
+            }
+          })
+        }
+
+
+
+        // 
+        if(showEditSetting){
+          showEditSetting.onclick = function(){
+            if(settingBlock.matches('.shown')){
+              settingBlock.classList.remove('shown');
+              editSettingBlock.classList.add('shown')
+            }
+          }
+        }
+
+
+
+        // 
+        if(showExtendSetting){
+          showExtendSetting.onclick = function(){
+            extendSettingBlock.firstElementChild.classList.toggle('open')
+          }
+        }
       }
 
       // middle column
@@ -302,6 +398,52 @@ document.addEventListener("DOMContentLoaded", function () {
         });
       }
 
+      // popup add contact
+      if(main){
+        var addContact = main.querySelectorAll('.AddContact');
+        if(popupAddContact){
+          popupAddContact.forEach(function(a){
+            var showPopupAddContact = a.querySelector('.NewContactModal');
+            addContact.forEach(function(b){
+              b.onclick = function(){
+                showPopupAddContact.classList.add('open');
+                showPopupAddContact.classList.add('shown');
+              }
+            })
+  
+            var cancelAddContact = a.querySelector('.Cancel');
+            cancelAddContact.onclick = function(){
+              showPopupAddContact.classList.remove('open');
+              showPopupAddContact.classList.remove('shown');
+            }
+          })
+        }
+
+      }
+
+
+      // popup new contact
+      if(main){
+        var newContact = main.querySelectorAll('.NewContact');
+        if(popupNewContact){
+          popupNewContact.forEach(function(a){
+            var showPopupNewContact = a.querySelector('.NewContactModal');
+            newContact.forEach(function(b){
+              b.onclick = function(){
+                showPopupNewContact.classList.add('open');
+                showPopupNewContact.classList.add('shown');
+              }
+            })
+  
+            var cancelNewContact = a.querySelector('.Cancel');
+            cancelNewContact.onclick = function(){
+              showPopupNewContact.classList.remove('open');
+              showPopupNewContact.classList.remove('shown');
+            }
+          })
+        }
+
+      }
       // hide cac element khi click ra ngoai
       document.addEventListener("click", function (e) {
         if (dropdownMenu) {
